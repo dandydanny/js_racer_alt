e04ca980d58808df78186412d970de611671146b
dandydanny <dandydanny@gmail.com>
2014-02-25 13:54:57 -0600

    Add winning logic

--- racer.js
+++ racer.js
@@ -2,6 +2,30 @@ function Player(player_name){
 	this.position = 0;
 	this.name = player_name;
 	console.log(name);
+
+	console.log(track_length);
+
+	if (this.position == track_length) {
+		console.log("player 1 win");
+	}
+
+	this.advance = function()
+	{
+		console.log("advance() invoked for player: " + this.name);
+		this.position = this.position + 10;
+		//check if position >= track_length
+		console.log("track_length: " + track_length);
+		console.log("position: " + this.position);
+		if (this.position == track_length){
+			console.log(this.name + " wins!")
+			$('.status').html("<div class=\"status\">" + this.name + " Wins!</div>")
+		}
+	}
+
+	this.reset = function()
+	{
+		this.position = 0;
+	}
 }
 
 function resetRace(){
@@ -10,17 +34,15 @@ function resetRace(){
 	return position;
 }
 
-function advancePlayer(player){
-	$("p1");
-}
-
 $(document).ready(function() {
 
-	console.log("js ready")
+	console.log("js ready");
+
+	window.track_length = 100;
+	window.enable_race = true;
 
 	var p1 = new Player("Tommy");
 	var p2 = new Player("Aaron");
-
 
 	$("#reset-button").click(function(){
 		position = resetRace();
@@ -28,6 +50,14 @@ $(document).ready(function() {
 
 	console.log(p1);
 	console.log(p1.name);
+	console.log(p1.position);
+
+	$("#advance-button").click(function(){
+		p1.advance();
+		console.log(p1.position);
+		$(".p1").css("left", p1.position);
+	});
+
 	console.log(p1.position);
 
 });
